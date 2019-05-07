6aaa948656add3ef3a36acd33c7e6ec45db602aa
JasonPob <jasonpob@gmail.com>
2019-05-07 14:42:17 -0400

    heroku

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -21,27 +21,27 @@ module.exports = function (app) {
 
         var userData = req.body;
         var userScores = userData.scores;
-        var totalDifference;
+        var scoreDifference;
         for (var i = 0; i < friends.length; i++) {
             var currentFriend = friends[i];
-            totalDifference = 0;
+            scoreDifference = 0;
 
             console.log(currentFriend.name);
 
-            // We then loop through all the scores of each friend
+            //Scores per friend
             for (var j = 0; j < currentFriend.scores.length; j++) {
                 var currentFriendScore = currentFriend.scores[j];
                 var currentUserScore = userScores[j];
 
-                // We calculate the difference between the scores and sum them into the totalDifference
-                totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+                // We calculate the difference between the scores and sum them into the scoreDifference
+                scoreDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
             }
             // If the sum of differences is less then the differences of the current "best match"
-            if (totalDifference <= bestMatch.friendDifference) {
+            if (scoreDifference <= bestMatch.friendDifference) {
                 // Reset the bestMatch to be the new friend.
                 bestMatch.name = currentFriend.name;
                 bestMatch.photo = currentFriend.photo;
-                bestMatch.friendDifference = totalDifference;
+                bestMatch.friendDifference = scoreDifference;
             }
         }
 
